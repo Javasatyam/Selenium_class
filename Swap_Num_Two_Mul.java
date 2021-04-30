2014b5335a6cf48eb4bf29bf2a488f2091b189bd
Javasatyam <83443890+Javasatyam@users.noreply.github.com>
2021-04-30 11:03:29 +0530

    Add files via upload

---
+++ Swap_Num_Two_Mul.java
@@ -0,0 +1,16 @@
+package swap_of_number;
+
+public class Swap_Num_Two_Mul {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		a=a*b;
+		b=a/b;
+		a=a/b;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
